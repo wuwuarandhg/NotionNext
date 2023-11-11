b7bfae093d111ac4ad78a865f078f44d0deb893d
wuwuarandhg <98201994+wuwuarandhg@users.noreply.github.com>
2023-11-11 18:26:16 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,8 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+var umamiScript = document.createElement('script');
+// Set the script source and attributes
+umamiScript.src = 'https://umami.wuran.link/script.js';
+umamiScript.async = true;
+umamiScript.setAttribute('data-website-id', '29a4af49-f724-43a7-a625-7aa28c5f1875');
+document.head.appendChild(umamiScript);
+    
